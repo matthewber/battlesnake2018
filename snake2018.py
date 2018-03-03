bba684addbcd5267d8c95d2aa551246fdba5fd92
Matthew Berntsen <35281164+matthewber@users.noreply.github.com>
2018-03-03 11:27:04 -0800

    Update snake2018.py

--- snake2018.py
+++ snake2018.py
@@ -42,10 +42,10 @@ def move():
     # TODO: Do things with data
     
     directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    direction = 'up'
     print direction
     return {
-        'move': direction,
+        'move': 'up',
         'taunt': 'battlesnake-python!'
     }
 
