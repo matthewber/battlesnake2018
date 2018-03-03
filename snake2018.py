6ca31b43645ede065cea643c63da38b818e50126
Matthew Berntsen <35281164+matthewber@users.noreply.github.com>
2018-03-03 11:24:15 -0800

    Update snake2018.py

--- snake2018.py
+++ snake2018.py
@@ -3,54 +3,59 @@ import os
 import random
 
 
+
 @bottle.route('/')
 def static():
-    return " "
+    return "the server is running"
 
 
 @bottle.route('/static/<path:path>')
 def static(path):
-        return bottle.static_file(path, root='static/')
+    return bottle.static_file(path, root='static/')
+
 
 @bottle.post('/start')
 def start():
-        data = bottle.request.json
-        game_id = data.get('game_id')
-        board_width = data.get('width')
-        board_height = data.get('height')
+    data = bottle.request.json
+    game_id = data.get('game_id')
+    board_width = data.get('width')
+    board_height = data.get('height')
 
-        head_url = '%s://%s/static/head.png' % (
-            bottle.request.urlparts.scheme,
-            bottle.request.urlparts.netloc
-        )
+    head_url = '%s://%s/static/head.png' % (
+        bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc
+    )
 
-        return {
+    # TODO: Do things with data
 
-         'color': '#FF0000',
-         'secondary_color': '#00FF00',
-         'head_url': 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAdMAAAAJDBlMDJjMjI2LTgyMWItNDc5My1iM2Q4LWQ1NDg2MWQ5YTIwNg.jpg',
-         'taunt': 'I hope I remember how to turn ',
-         'head_type': 'safe',
-         'tail_type': 'skinny'
-        }
+    return {
+        'color': '#00FF00',
+        'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
+        'head_url': head_url
+    }
+
 
 @bottle.post('/move')
 def move():
     data = bottle.request.json
+
+    # TODO: Do things with data
+    
     directions = ['up', 'down', 'left', 'right']
-    direction = 'up' #random.choice(directions)
-    print directions
+    direction = random.choice(directions)
+    print direction
     return {
         'move': direction,
-        'taunt': 'I hope I remember how to turn'
+        'taunt': 'battlesnake-python!'
     }
 
+
+# Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
 if __name__ == '__main__':
     bottle.run(
         application,
-        host=os.getenc('IP', '0.0.0.0'),
+        host=os.getenv('IP', '0.0.0.0'),
         port=os.getenv('PORT', '8080'),
-        debug = True
-    )
+        debug = True)
